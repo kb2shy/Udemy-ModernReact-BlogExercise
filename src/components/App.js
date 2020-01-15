75b55d12c999dc54607bb6595777c9ea887ff479
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-15 09:52:42 -0800

    created PostList component and fetchPosts action

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import PostList from './PostList';
+
 class App extends Component {
 
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <PostList />
+            </div>
         )
     }
 }
