52464fedf6583cef9f75a2d04f3b710321dc6e57
Chhaian Pin <chhaianpin@hotmail.com>
2020-01-15 21:34:43 -0800

    completed blog project

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,28 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPosts = () => dispatch => {
-    jsonPlaceholder.get('/posts')
-    .then(response => {
-        return dispatch({ type: 'FETCH_POSTS', payload: response.data })
-    })
+// export const fetchPosts = () => dispatch => {
+//     jsonPlaceholder.get('/posts')
+//     .then(response => {
+//         return dispatch({ type: 'FETCH_POSTS', payload: response.data })
+//     })
+// }
+
+// export const fetchUser = (id) => dispatch => {
+//     jsonPlaceholder.get(`/users/${id}`)
+//     .then(response => {
+//         return dispatch({ type: 'FETCH_USER', payload: response.data });
+//     })
+// };
+
+export const fetchPosts = () => async dispatch => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 }
 
-export const fetchUser = (id) => dispatch => {
-    jsonPlaceholder.get(`/users/${id}`)
-    .then(response => {
-        return dispatch({ type: 'FETCH_USER', payload: response.data });
-    })
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`)
+    dispatch({ type: 'FETCH_USER', payload: response.data });
 };
 
 // memoized version
@@ -26,8 +36,15 @@ export const fetchUser = (id) => dispatch => {
 //     })
 // });
 
-export const fetchPostsAndUsers = () => async dispatch => {
-    console.log('about to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
-    console.log('fetched posts');
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
 }
